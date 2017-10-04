14ef09fb13f1d9375c7819156fe74c823df8f034
David Chung <david.chung@docker.com>
2017-10-03 21:53:05 -0700

    update schema
    Signed-off-by: David Chung <david.chung@docker.com>

--- latest/kube/kube-managers-init.sh
+++ latest/kube/kube-managers-init.sh
@@ -6,13 +6,19 @@ set -o xtrace
 {{/* Install Kubeadm */}}
 {{ include "install-kubeadm.sh" }}
 
+{{ if BOOTSTRAP }}
+# Bootstrap node
 kubeadm init --skip-preflight-checks --token {{ KUBEADM_JOIN_TOKEN }}
 export KUBECONFIG=/etc/kubernetes/admin.conf
 {{ if ADDON "network" }}
     kubectl apply -f {{ ADDON "network" }}
-{{ else }}
 {{ end }}
 {{ if ADDON "visualise" }}
     kubectl apply -f {{ ADDON "visualise" }}
-{{ else }}
+{{ end }}
+{{ end }}{{/* bootstrap */}}
+
+{{ if WORKER }}
+# Worker mode -- if a node with a logical ID isn't part of the control plane.
+kubeadm join --skip-preflight-checks --token {{ KUBEADM_JOIN_TOKEN }} {{ KUBE_JOIN_IP }}:{{ BIND_PORT }}
 {{ end }}
