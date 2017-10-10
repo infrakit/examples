7f723705c3413e19505f5e6e879e51741439dcd5
David Chung <david.chung@docker.com>
2017-10-10 00:52:26 -0700

    Kube example (#12)
    Signed-off-by: David Chung <david.chung@docker.com>

--- latest/swarm/infrakit.sh
+++ latest/swarm/infrakit.sh
@@ -28,7 +28,7 @@ docker run -d --restart always --name infrakit -p 24864:24864 {{ $dockerMounts }
        -e INFRAKIT_ADVERTISE={{ var `/local/swarm/manager/logicalID` }}:24864 \
        -e INFRAKIT_TAILER_PATH=/var/log/cloud-init-output.log \
        {{$dockerImage}} \
-       infrakit plugin start manager group vars aws combo swarm time tailer ingress \
+       infrakit plugin start manager group vars aws combo swarm time tailer ingress kubernetes \
        --log 5 --log-debug-V 900
 
 # Need a bit of time for the leader to discover itself
@@ -41,6 +41,7 @@ docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
        cluster/size={{ var `/cluster/size` }} \
        infrakit/config/root={{ var `/infrakit/config/root` }} \
        infrakit/docker/image={{ var `/infrakit/docker/image` }} \
+       infrakit/metadata/configURL={{ var `/infrakit/metadata/configURL` }} \
        provider/image/hasDocker={{ var `/provider/image/hasDocker` }} \
 
 
