2c73dec226cf38ff8863327b9b98f9f03c3993b6
David Chung <david.chung@docker.com>
2017-10-10 17:15:07 -0700

    block on leader only
    Signed-off-by: David Chung <david.chung@docker.com>

--- latest/swarm/infrakit.sh
+++ latest/swarm/infrakit.sh
@@ -34,10 +34,11 @@ docker run -d --restart always --name infrakit -p 24864:24864 {{ $dockerMounts }
 # Need a bit of time for the leader to discover itself
 sleep 60
 
-echo "Block here to demonstrate the blocking metadata and asynchronous user update"
+{{ if eq (var `/local/swarm/manager/logicalID`) (var `/cluster/swarm/join/ip`) }}
+echo "Block here to demonstrate the blocking metadata and asynchronous user update... Only on first node."
 docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
-       infrakit vars metadata cat /usr/token --retry 1s --timeout 60m \
-
+       infrakit template 'str://Please enter usr/token via CLI: {{ metadata `vars/usr/token` `1s` `60m` }}' \
+{{ end }}
 
 echo "Update the vars in the metadata plugin -- we put this in the vars plugin for queries later."
 docker run --rm {{$dockerMounts}} {{$dockerEnvs}} {{$dockerImage}} \
